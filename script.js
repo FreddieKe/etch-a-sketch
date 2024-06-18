a3a8c4c77e4976ace86037770367024863fce978
Freddie <freddie.kenyon98@gmail.com>
2024-06-18 09:19:04 +0100

    Add row limit and multicolour function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let container = document.querySelector("container");
+let container = document.querySelector("#container");
 let gridNumber = 4;
 
 function drawBoard(gridNumber) {
@@ -6,7 +6,7 @@ function drawBoard(gridNumber) {
     for (let i = 0; i < gridNumber; i++) {
         let div = document.createElement("div");
         div.classList.add("row");
-        for (let i = 0; i < 4; i++) {
+        for (let i = 0; i < gridNumber; i++) {
             let childDiv = document.createElement("div");
             childDiv.classList.add("child");
             childDiv.id = `box${id}`;
@@ -16,14 +16,29 @@ function drawBoard(gridNumber) {
     }}
 
 function deleteBoard() {
-    let boxes =
+    let boxes = document.querySelectorAll(".child");
+    let rows = document.querySelectorAll(".row");
+    boxes.forEach((currentValue, currentIndex) => {
+        boxes[currentIndex].remove();
+    }) 
+    rows.forEach((currentValue, currentIndex) => {
+        rows[currentIndex].remove();
+    })}
+
+function chooseRandomRGB() {
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    console.log(`rgb(${r},${g},${b})`);
+    return `rgb(${r},${g},${b})`
 }
 
 // Apply hover effects
 container.addEventListener("mouseover", (e) => {
     if (e.target.id.substr(0,3) === "box") {
     let hoveredBox = document.querySelector(`#${e.target.id}`);
-    hoveredBox.style.backgroundColor = "red";}
+    hoveredBox.style.backgroundColor = `${chooseRandomRGB()}`;
+    }
 })
 
 
@@ -33,12 +48,12 @@ let gridButton = document.querySelector("#grid-button");
 gridButton.addEventListener("click", () => {
     let keepGoing = true;
     while (keepGoing === true) {
-        gridNumber = Number(prompt("How many rows do you want? (Maximum 30)"));
+        gridNumber = Number(prompt("How many rows do you want? (Maximum 100)"));
         if (typeof(gridNumber) === "number"
-            && gridNumber <= 30) {
+            && gridNumber <= 100) {
+                deleteBoard();
                 return drawBoard(gridNumber);
         } else {
             alert("ERROR");
-        }
-    }})
+        }}})
 
